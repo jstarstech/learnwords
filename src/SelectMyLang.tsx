40dd5c96547110f61576c02bd101bd9b3bf2bba2
Maksim C <jstars.tech@gmail.com>
2023-08-28 15:38:13 +0300

    Language change confirm dialog

--- src/SelectMyLang.tsx
+++ src/SelectMyLang.tsx
@@ -2,17 +2,42 @@ import FormControl, { FormControlProps } from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { StateContext } from "./State";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContentText from "@mui/material/DialogContentText";
+import Button from "@mui/material/Button";
 
 export default function SelectMyLang(
   props: FormControlProps & { selectSize?: "small" | "medium" }
 ) {
   const { state, stateDispatch } = useContext(StateContext);
   const { selectSize, ..._props } = props;
+  const [open, setOpen] = useState(false);
+  const selectedLang = useRef(state.lang);
+
+  const handleConfirm = () => {
+    stateDispatch({ type: "getLearnWords", lang: selectedLang.current });
+    setOpen(false);
+  };
+
+  const handleClose = () => {
+    selectedLang.current = state.lang;
+    setOpen(false);
+  };
 
   function handleSelectLang(event: SelectChangeEvent) {
-    stateDispatch({ type: "getLearnWords", lang: event.target.value });
+    if (state.progress === 0 && state.wordsStartIdx === 0) {
+      selectedLang.current = event.target.value;
+      stateDispatch({ type: "getLearnWords", lang: selectedLang.current });
+      return;
+    }
+
+    selectedLang.current = event.target.value;
+    setOpen(true);
   }
 
   return (
@@ -27,6 +52,30 @@ export default function SelectMyLang(
         <MenuItem value={"ua"}>Ukrainian</MenuItem>
         <MenuItem value={"ru"}>Russian</MenuItem>
       </Select>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Are you confirm language change?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Your learning progress will be lost on language change.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="success" onClick={handleClose}>
+            No
+          </Button>
+          <Button color="success" onClick={handleConfirm} autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </FormControl>
   );
 }
